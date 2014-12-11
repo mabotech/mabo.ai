3e82cd2858429e088bcb16ef34e25f60d8244bef
majj <aidear@163.com>
2014-12-11 19:53:40 +0800

    test ok

--- py/webservices/orgService.py
+++ py/webservices/orgService.py
@@ -3,6 +3,8 @@
 """
 IAMService
 """
+import time
+
 import xml.sax.saxutils as saxutils
 
 # post xml soap message
@@ -30,28 +32,44 @@ class IAMClient(object):
 
         print(self.conf)
 
-    def getUserByUsername(self):
+    def searchAll(self, startPage, pageSize ):
         
         #config = static.ERP_CONFIG #'SL 8.0'
         
-        SM_TEMPLATE = r"""<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.iamldap.foton.com.cn/">
-   <soapenv:Header>
-         <wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+        query = {"username":self.conf["Admin"],"password":self.conf["Admin_Password"], "nonce":self.conf["Nonce"], "startPage":startPage, "pageSize": pageSize}
+        
+        
+        SM_TEMPLATE = r"""<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sear="http://search.service.iam.foton.com/">
+       <soapenv:Header>
+
+      <wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+
          <wsse:UsernameToken wsu:Id="UsernameToken-1" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-            <wsse:Username>%(username)</wsse:Username>
-            <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">%(password)</wsse:Password>
-            <wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">+gyCOJlGPRu8sYOwP+D+Qw==</wsse:Nonce>
-            <wsu:Created>2014-12-06T01:49:02.953Z</wsu:Created>
+
+            <wsse:Username>%(username)s</wsse:Username>
+
+            <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">%(password)s</wsse:Password>
+
+            <wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">%(nonce)s</wsse:Nonce>
+
+            <wsu:Created>2012-07-06T01:49:02.953Z</wsu:Created>
+
          </wsse:UsernameToken>
+
       </wsse:Security>
-       </soapenv:Header>
+
+   </soapenv:Header>
    <soapenv:Body>
-      <ser:searchOrgByParentOrgid>
-         <l>1262678244351298191</l>
-      </ser:searchOrgByParentOrgid>
+      <sear:searchAll>
+         <arg0>%(startPage)s</arg0>
+         <arg1>%(pageSize)s</arg1>
+         <!--Optional:-->
+         <arg2>ou</arg2>
+         <arg3>true</arg3>
+      </sear:searchAll>
    </soapenv:Body>
-</soapenv:Envelope>
-"""
+</soapenv:Envelope>"""  % query
 
         SoapMessage = SM_TEMPLATE
 
@@ -62,7 +80,7 @@ class IAMClient(object):
         print(host)
         webservice = httplib.HTTP(host)
         
-        service = self.conf["Service"]
+        service = self.conf["Service2"]
         
         url = "/IAMService/services/soap/%s" %(service)
         
@@ -79,13 +97,16 @@ class IAMClient(object):
         # get the response
 
         statuscode, statusmessage, header = webservice.getreply()
-        #print "Response: ", statuscode, statusmessage
+        print "Response: ", statuscode, statusmessage, startPage
         #print "headers: ", header
-        
+        #print dir(webservice)
         res = webservice.getfile().read()
         
-        print res
-        
+        fn = "%d.xml" %(time.time())
+        #print res
+        #with open(fn, 'w') as fh:
+        #    fh.write(res)
+            
         return  res #self.parseSessionToken(res)
     
     
@@ -127,12 +148,66 @@ class IAMClient(object):
         else:
             return "F"    
 
+def get_element_text(element, node):
+    
+    v = element.xpath(node)
+    
+    if len(v)>0:
+        #print v[0].text.encode("utf8")
+        return v[0].text.encode("utf8")
+    else:
+        return ""
 
 def main():
     
     cm = IAMClient()
     
-    xmlstr = cm.getUserByUsername()
+    fh = open("id3.csv","w")
+    
+    for i in range(1, 20):
+    
+        xmlstr = cm.searchAll(i,10)
+        
+        string_file = StringIO(xmlstr.replace('soap:','').replace("ns2:",""))
+        
+        
+
+        #root = etree.fromstring(xml)
+        tree = etree.parse(string_file)
+        
+        
+
+        resp = None
+        
+        
+
+        for element in tree.xpath('/Envelope/Body/searchAllResponse/return/userData'):
+            #resp = element[0][1].text
+            #print "\n"
+            v1 = get_element_text(element, "cn")
+            
+            v2 = get_element_text(element, "mail")
+
+            v3 = get_element_text(element, "fotonAppAtt37")
+
+            v4 = get_element_text(element, "mobile")
+     
+            v5 = get_element_text(element, "telephoneNumber")
+
+            v6 = get_element_text(element, "uid")
+
+            v7 = get_element_text(element, "ou")
+            #print userPassword[0].text,
+            
+            
+            x = "%s,%s,%s,%s,%s,%s,%s\n" % (v1, v2, v3, v4, v5, v6, v7)
+            
+            fh.write(x)
+            
+        time.sleep(0.5)
+        
+
+    fh.close()    
     
     """
     
